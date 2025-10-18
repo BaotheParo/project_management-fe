5df8ce17ade4acb6aafc3271185df2b6d2fd2fa9
Trương Minh Nhật <fleeforezz@gmail.com>
2025-10-18 21:58:56 +0700

    Create sidebar for sc staff

--- src/layout/SCStaffLayout.jsx
+++ src/layout/SCStaffLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from '../components/sc-staff/Sidebar'
 
 export default function SCStaffLayout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <header className="p-4 bg-white shadow">SC Staff header</header>
+      <Sidebar />
       <main className="p-6">{children}</main>
     </div>
   )
