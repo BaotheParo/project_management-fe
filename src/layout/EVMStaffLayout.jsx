36434e11c0d189dbbdfd267391ddb6a3ac6f4727
minhtrieu0712 <156668558+minhtrieu0712@users.noreply.github.com>
2025-11-05 19:03:34 +0700

    EVM staff ui
    completed dashboard, policy, payment, campaign still have 2 pages(.jsx) not available

--- src/layout/EVMStaffLayout.jsx
+++ src/layout/EVMStaffLayout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import EVMSidebar from "../features/dashboard/evm-staff/components/EVMSidebar";
 
-export default function EVMStaffLayout({ children }) {
+export default function EVMStaffLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <header className="p-4 bg-white shadow">EVM Staff header</header>
-      <main className="p-6">{children}</main>
+      <EVMSidebar />
+      <main className="ml-64 p-6">
+        {/* Header inside content area*/}
+
+        <Outlet />
+      </main>
     </div>
-  )
+  );
 }
