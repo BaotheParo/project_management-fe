adc9cee97ba0209c3993a9a95314060c3226c195
ToilaHun <viethunglist@gmail.com>
2025-11-10 21:57:38 +0700

    fixxx
    giờ nó sẽ dựa vào ngày mà quyết định thứ tự approve nhé

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests to backend to avoid CORS issues in development
       '/api': {
-        target: 'https://dev-be-wm.fleeforezz.site',
+        target: 'https://dev-be-wm.hikarimoon.pro',
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path, // Keep /api in the path
